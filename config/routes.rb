cc69d08e2ecdbce3452e28d43b0d005f4919e829
Jake <github@jakewendt.com>
2017-02-27 11:47:10 -0800

    Deving actual usage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 #  get 'reports/montly_birth_counts'
-	get 'reports/vaccination_count_by_year_month'
+
+	resources :reports, only: :index do
+		collection do
+			get :vaccination_count_by_year_month
+		end
+	end
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :observations, only: :index
