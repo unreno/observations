af269d37ab6fdb72e7ba260ad5b12de795c28cee
Jake <github@jakewendt.com>
2017-02-28 13:21:58 -0800

    Using plotly.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :observations, only: :index
 
-	root :to => "observations#index"
+	root :to => "reports#vaccination_count_by_year_month"
+#	root :to => "observations#index"
 
 end
